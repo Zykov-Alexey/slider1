0c9175f7d50e113f23e8f1718c46fbdd0a651766
Alexey Zykov <zykov.alexey@gmail.com>
2020-04-15 10:55:11 +0300

    Slider JS - DZ - переделал

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,107 +1,108 @@
-(function() {
-  let carousel = document.querySelector('.carousel__wrapper');
-  let slides = document.querySelectorAll('.slides__item');
-  let pauseBtn = document.querySelector('#pause');
-  let prevBtn = document.querySelector('#previous');
-  let nextBtn = document.querySelector('#next');
-  let controls = document.querySelector('.controls');
-  let indicatorsContainer = document.querySelector('.indicators');
-  let indicators = document.querySelectorAll('.indicators__item');
+let container = document.querySelector('.container');
+let slides = document.querySelectorAll('.slides__item');
+let pauseBtn = document.querySelector('#pause');
+let nextBtn = document.querySelector('#next');
+let prevBtn = document.querySelector('#previous');
 
-  let currentSlide = 0;
-  let interval = 3000;
-  let isPlaying = true;
-  let slidesLength = slides.length;
-  let swipeStartX = null;
-  let swipeEndX = null;
-  let timerID = null;
+let controls = document.querySelector('.controls__item');
 
-  const LEFT_ARROW = 'ArrowLeft';
-  const RIGHT_ARROW = 'ArrowRight';
-  const SPACE = ' ';
+let indicatorsContainer = document.querySelector('.indicators');
+let indicators = document.querySelectorAll('.indicators__item');
 
-  let goToSlide = (n) => {
-    slides[currentSlide].classList.toggle('active');
-    indicators[currentSlide].classList.toggle('active');
-    currentSlide = (n + slidesLength) % slidesLength;
-    slides[currentSlide].classList.toggle('active');
-    indicators[currentSlide].classList.toggle('active');
-  };
+let currentSlide = 0;
+let isPlaying = true;
+let interval = 2000;
+let slidesLength = slides.length; 
 
-  let goToNextSlide = () => {
-    goToSlide(currentSlide + 1);
-  };
+let swipeStartX = null;
+let swipeEndX = null;
+let timerID = null;
 
-  let goToPrevSlide = () => {
-    goToSlide(currentSlide - 1);
-  };
 
-  let pause = () => {
-    if (isPlaying) { 
-      pauseBtn.innerHTML = 'Play';
-      isPlaying = !isPlaying;
-      clearInterval(timerID);
-    }
-  };
+const SPACE = ' ';
+const LEFT_ARROW = 'ArrowLeft';
+const RIGHT_ARROW = 'ArrowRight';
 
-  let play = () => {
-    pauseBtn.innerHTML = 'Pause';
-    isPlaying = isPlaying;
-    timerID = setInterval(goToNextSlide, interval);
-  };
 
-  let clickPause = () => isPlaying ? pause() : play();
+controls.style.display = 'flex';
+indicatorsContainer.style.display = 'flex';
 
-  let clickNext = () => {
+let goToSlide = (n) => {
+  slides[currentSlide].classList.toggle('active');
+  indicators[currentSlide].classList.toggle('active');
+  currentSlide = (n + slidesLength) % slidesLength;
+  slides[currentSlide].classList.toggle('active');
+  indicators[currentSlide].classList.toggle('active');
+};
+
+let gotoNextSlide = () => {
+  goToSlide(currentSlide + 1);
+};
+  
+let gotoPrevSlide = () => {
+  goToSlide(currentSlide - 1);
+};
+
+let pause = () => {
+  if (isPlaying) {
+    pauseBtn.innerHTML = 'Play';
+    isPlaying = !isPlaying;
+    clearInterval(timerID);
+  }
+};
+
+let play = () => {
+  pauseBtn.innerHTML = 'Pause';
+  isPlaying = true;
+  timerID = setInterval(gotoNextSlide, interval);
+};
+
+let clickPause = () => isPlaying ? pause() : play();
+
+let clickNext = () => {
+  pause();
+  gotoNextSlide();
+};
+
+let clickPrev = () => {
+  pause();
+  gotoPrevSlide();
+};
+
+let clickIndicatorBtn = (e) => {
+  let target = e.target;
+    if (target.classList.contains('indicators__item')) {
     pause();
-    goToNextSlide();
-  };
+    goToSlide(+target.getAttribute('data-slide-to'));
+  }
+};
 
-  let clickPrev = () => {
-    pause();
-    goToPrevSlide();
-  };
+let pressKey = (e) => {
+  if (e.key === LEFT_ARROW) clickPrev();
+  if (e.key === RIGHT_ARROW) clickNext();
+  if (e.key === SPACE) clickPause();
+};
 
-  let clickIndicatorBtn = (e) => {
-    let target = e.target;
-    if (+target.classList.contains('indicators__item')) {
-      pause();
-      goToSlide(+target.getAttribute('data-slide-to'));
-    }
-  };
+let swipeStart = (e) => {
+  swipeStartX = e.changedTouches[0].pageX;
+};
 
-  let pressKey = (e) => {
-    if (e.key === LEFT_ARROW) clickPrev();
-    if (e.key === RIGHT_ARROW) clickNext();
-    if (e.key === SPACE) clickPause();
-  };
+let swipeEnd = (e) => {
+  swipeEndX = e.changedTouches[0].pageX;
+  swipeStartX - swipeEndX < 100 && clickPrev();
+  swipeStartX - swipeEndX > -100 && clickNext();
+};
 
-  let swipeStart = (e) => {
-    swipeStartX = e.changedTouches[0].pageX;
-  };
 
-  let swipeEnd = (e) => {
-    swipeEndX = e.changedTouches[0].pageX;
-    swipeStartX - swipeEndX > 100 && clickNext();
-    swipeStartX - swipeEndX < -100 && clickPrev();
-  };
 
-  let setListeners = () => {
-    pauseBtn.addEventListener('click', clickPause);
-    nextBtn.addEventListener('click', clickNext);
-    prevBtn.addEventListener('click', clickPrev);
-    indicatorsContainer.addEventListener('click', clickIndicatorBtn);
-    document.addEventListener('keydown', pressKey);
-    carousel.addEventListener('touchstart', swipeStart);
-    carousel.addEventListener('touchend', swipeEnd);
-  };
+pauseBtn.addEventListener('click', clickPause);
+nextBtn.addEventListener('click', clickNext);
+prevBtn.addEventListener('click',clickPrev);
 
-  let init = () => {
-    controls.style.display = 'flex';
-    indicatorsContainer.style.display = 'flex';
-    setListeners();
-    timerID = setInterval(goToNextSlide, interval);
-  };
+indicatorsContainer.addEventListener('click', clickIndicatorBtn);
+document.addEventListener('keydown', pressKey);
 
-  init();
-}());
+container.addEventListener('touchstart', swipeStart);
+container.addEventListener('touchend', swipeEnd);
+
+timerID = setInterval(gotoNextSlide, interval);
